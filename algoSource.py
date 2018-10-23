115f3b732070fc5dc227f8b0f42454d16e72b857
error0001 <error00010001@gmail.com>
2018-10-23 12:03:09 +0300

    defence

--- algoSource.py
+++ algoSource.py
@@ -1,20 +1,22 @@
 import os
 #SCF == smallest common factor == naimen'shiy obshiy delitel' == NOD
-#src of material : https://younglinux.info/algorithm
+#src of material : https://ru.wikibooks.org/wiki/%D0%A0%D0%B5%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8_%D0%B0%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC%D0%BE%D0%B2/%D0%90%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC_%D0%95%D0%B2%D0%BA%D0%BB%D0%B8%D0%B4%D0%B0#Python
 #Algorithm of finder "SCF" across division
 def Evkl_Division(inA, inB):
-  a = int(inA)
-  b = int(inB)
-  try:
-    while a!=0 and b!=0:
-      if a > b:
-        a = a % b
-      else:
-        b = b % a
-    print (a+b)
-  except IOError:
-    print("ERROR : PROBLEM WITH VALUES OF INPUT")
-  
+  if int(inA) > 0 and int(inB) > 0:
+    try:
+      a = int(inA)
+      b = int(inB)
+      while a!=0 and b!=0:
+        if a > b:
+          a = a % b
+        else:
+          b = b % a
+      print (a+b)
+    except IOError:
+      print("ERROR : PROBLEM WITH VALUES OF INPUT")
+  else:
+    print("Warning : ever value is NULL or NEGATIVE, check input")
 
 if __name__ == '__main__':
   Evkl_Division(input("Enter a: "), input("Enter b: "))
